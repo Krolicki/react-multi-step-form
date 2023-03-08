87730dd475c5300d6db5dc0167009f5612efe838
Jakub Królicki <jakub.krolicki.k@gmail.com>
2023-03-08 22:33:33 +0100

    steps descriptions

--- src/components/Form/Progress/Progress.js
+++ src/components/Form/Progress/Progress.js
@@ -4,13 +4,13 @@ export const Progress = ({currentStep}) => {
     return(
         <div className='progress'>
             <div className={`progress-item ${currentStep > 1 ? "completed" : currentStep === 1 ? "active" : ""}`} >
-                <span className="progress-number">1</span>
+                <span className="progress-number" item-text="First step">1</span>
             </div>
             <div className={`progress-item ${currentStep > 2 ? "completed" : currentStep === 2 ? "active" : ""}`} >
-                <span className="progress-number">2</span>
+                <span className="progress-number" item-text="Step two">2</span>
             </div>
             <div className={`progress-item ${currentStep > 3 ? "completed" : currentStep === 3 ? "active" : ""}`} >
-                <span className="progress-number">3</span>
+                <span className="progress-number" item-text="Final step">3</span>
             </div>
         </div>
     )
