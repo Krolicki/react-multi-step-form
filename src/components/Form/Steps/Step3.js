504dd5f09db6b377e182deb48e6df5622d73a842
Jakub Królicki <jakub.krolicki.k@gmail.com>
2023-03-12 15:35:59 +0100

    final step

--- src/components/Form/Steps/Step3.js
+++ src/components/Form/Steps/Step3.js
@@ -4,27 +4,30 @@ export const Step3 = () => {
     return(
         <div className='step'>
             <span className='step-title'>Agreements</span>
-            <input
-                type='email'
-                name='mail'
-                placeholder=' '
-            />
-            <label for="mail">Email Address</label>
-            <input
-                type='password'
-                name='password'
-                placeholder=' '
-            />
-            <label for="password">Password</label>
-            <input
-                type='password'
-                name='confirmPass'
-                placeholder=' '
-            />
-            <label for="confirmPass">Confirm Password</label>
+            <span className='step-checkbox'>
+                <input
+                    type='checkbox'
+                    name='terms-consent'
+                />
+                <label htmlFor='terms-consent'>I have read and accept the terms of service</label>
+            </span>
+            <span className='step-checkbox'>
+                <input
+                    type='checkbox'
+                    name='data-consent'
+                />
+                <label htmlFor='data-consent'>I consent to the processing of my personal data necessary for registration</label>
+            </span>
+            <span className='step-checkbox'>
+                <input
+                    type='checkbox'
+                    name='marketing-consent'
+                />
+                <label htmlFor='marketing-consent'>(optional) I consent to the processing of my personal data for marketing purposes</label>
+            </span>
             <span className='step-buttons'>
-                <button type="button" >Previous step</button>
-                <button type="button" >Next step</button>
+                    <button type="button" >Previous step</button>
+                    <button type="button" >Submit</button>
             </span>
         </div>
     )
